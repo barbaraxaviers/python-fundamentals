c2e4f661e04a28ef99970ab41fc9e3b7931b94be
Bárbara Xavier <barbaramv.integrativa@gmail.com>
2026-04-08 20:08:47 -0300

    Add files via upload

---
+++ ex4_somando_adding_adunarea.py
@@ -0,0 +1,9 @@
+#somando dois numeros
+#adding two numbers
+#adunarea a doua numere
+
+n1 = float(input("Digite um valor:")) #tipo primitivo antes do input
+n2 = float(input("Digite mais um valor:"))
+s = n1 + n2
+print(f"A soma de {n1} e de {n2} é {s}.")
+print("A soma entre {} e {} é igual a {}!".format(n1,n2,s))
