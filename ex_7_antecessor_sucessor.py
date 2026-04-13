4766c475eac02bafdcc74f3bfacb54e6d57fd4aa
Bárbara Xavier <barbaramv.integrativa@gmail.com>
2026-04-13 20:03:21 -0300

    Add files via upload

---
+++ ex_7_antecessor_sucessor.py
@@ -0,0 +1,12 @@
+##Antecessor e Sucessor / Predecessor and Successor / Predecesor și Succesor
+
+n = int(input('Digite um numero:'))
+a = n - 1
+s = n + 1
+print(f'Analisando o valor {n}, seu antecessor é {a} e seu sucessor é {s}')
+print("Analisando o valor {}, seu antecessor é {} e seu sucessor é {}".format(n,a,s))
+#ou
+n1 = int(input('Digite um numero:'))
+print ('Analisando o valor {}, seu antecessor é {} e o seu sucessor é {}'.format(n1,(n1-1),(n1+1)))
+print(f'Analisando o valor de {n1}, seu antecessor é {n1-1} e o seu sucessor é {n1+1}')
+#quanto menos variaveis, menos memoria no seu dispositivo 
